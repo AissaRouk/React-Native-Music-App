cf4b300e6a0f10b0a0046645ee6d9c71bf460637
AissaRouk <aissa.rouk@gmail.com>
2024-02-04 21:18:13 +0200

    Mockup Data

---
+++ Data/mockupSongs.ts
@@ -0,0 +1,45 @@
+import { Song } from "../Types/Song";
+
+const mockupSongs: Song[] = [
+  {
+    id: "1",
+    title: "Song Title 1",
+    artist: "Artist 1",
+    duration: 180,
+    album: "Album 1",
+    releaseYear: 2022,
+  },
+  {
+    id: "2",
+    title: "Song Title 2",
+    artist: "Artist 2",
+    duration: 240,
+    album: "Album 2",
+    releaseYear: 2020,
+  },
+  {
+    id: "3",
+    title: "Song Title 3",
+    artist: "Artist 3",
+    duration: 200,
+    releaseYear: 2019,
+  },
+  {
+    id: "4",
+    title: "Song Title 4",
+    artist: "Artist 1",
+    duration: 210,
+    album: "Album 3",
+    releaseYear: 2021,
+  },
+  {
+    id: "5",
+    title: "Song Title 5",
+    artist: "Artist 4",
+    duration: 190,
+    releaseYear: 2020,
+  },
+  // Add more songs as needed
+];
+
+export default mockupSongs;
