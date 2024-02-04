954d3b4de5cca75ada77ce8d4d2390ce904044f3
AissaRouk <aissa.rouk@gmail.com>
2024-02-04 21:18:36 +0200

    Created song container

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,10 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  Image,
+} from "react-native";
+import { Dimensions } from "react-native";
+import mockupSongs from "../Data/mockupSongs";
+import formatDuration from "../Utils/formatDuration";
+
+const { height, width } = Dimensions.get("window");
 
 export default function MainScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Header</Text>
+      <ScrollView
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+          borderColor: "black",
+          borderWidth: 1,
+          width: width,
+        }}
+      >
+        {mockupSongs.map((item) => (
+          <View style={styles.songContainer}>
+            <Image source={require("../Data/Images/NoteImage.jpeg")} />
+            <Text>{item.title}</Text>
+            <Text>{item.artist}</Text>
+            <Text>{formatDuration(item.duration)}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -14,5 +45,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: StatusBar.currentHeight || 0,
+  },
+  //song container styles
+  songContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
   },
 });
