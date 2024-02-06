a76a75adf81d69a05a9f2c7039f94ed946328b1a
AissaRouk <aissa.rouk@gmail.com>
2024-02-06 12:24:14 +0200

    Song cointainers' style completed

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -10,6 +10,7 @@ import {
 import { Dimensions } from "react-native";
 import mockupSongs from "../Data/mockupSongs";
 import formatDuration from "../Utils/formatDuration";
+import Icon from "react-native-vector-icons/Entypo";
 
 const { height, width } = Dimensions.get("window");
 
@@ -27,11 +28,39 @@ export default function MainScreen() {
         }}
       >
         {mockupSongs.map((item) => (
-          <View style={styles.songContainer}>
-            <Image source={require("../Data/Images/NoteImage.jpeg")} />
-            <Text>{item.title}</Text>
-            <Text>{item.artist}</Text>
-            <Text>{formatDuration(item.duration)}</Text>
+          <View style={styles.songContainer} key={item.id}>
+            <View style={{ flexDirection: "row" }}>
+              <Image
+                source={require("../Data/Images/NoteImage.jpeg")}
+                resizeMode="contain"
+                style={{
+                  height: 50,
+                  width: 50,
+                  borderRadius: 9,
+                }}
+              />
+              <View
+                style={{
+                  marginHorizontal: 10,
+                  justifyContent: "center",
+                }}
+              >
+                <Text style={{ fontWeight: "600", marginBottom: 3 }}>
+                  {item.title}
+                </Text>
+                <View style={{ flexDirection: "row" }}>
+                  <Text>
+                    {item.artist} - {formatDuration(item.duration)}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <Icon
+              name="dots-three-horizontal"
+              size={18}
+              color={"grey"}
+              style={{ marginHorizontal: 10 }}
+            />
           </View>
         ))}
       </ScrollView>
@@ -41,7 +70,6 @@ export default function MainScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -49,7 +77,17 @@ const styles = StyleSheet.create({
   },
   //song container styles
   songContainer: {
+    flex: 1,
     flexDirection: "row",
     marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderColor: "black",
+    borderWidth: 1,
+    paddingVertical: 15,
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    height: 80,
+    width: width * 0.9,
   },
 });
