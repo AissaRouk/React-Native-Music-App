fec0118cee3d5035e489a7178cf59c573717865a
AissaRouk <aissa.rouk@gmail.com>
2024-02-16 19:33:30 +0200

    Added play properties

--- Context/Context.tsx
+++ Context/Context.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Song } from "../Types/Song";
-import { Alert } from "react-native";
 
 // Context prop types
 interface ContextProps {
@@ -9,6 +8,10 @@ interface ContextProps {
   setPlayingSong: (song: Song) => void;
   currentPlayingTime: number;
   setCurrentPlayingTime: (number: number) => void;
+  play: () => void;
+  pause: () => void;
+  isPlaying: boolean;
+  setIsPlaying: (bool: boolean) => void;
 }
 
 // Define the context
@@ -17,12 +20,17 @@ export const AppContext = createContext<ContextProps>({
   setPlayingSong: () => {},
   setCurrentPlayingTime: () => {},
   currentPlayingTime: undefined,
+  play: () => {},
+  pause: () => {},
+  isPlaying: false,
+  setIsPlaying: () => {},
 });
 
 // Define the ContextProvider component
 export function ContextProvider({ children }: { children: React.ReactNode }) {
   const [currentSong, setCurrentSong] = useState<Song | undefined>();
   const [currentPlayingTime, setCurrentPlayingTime] = useState<number>(0);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   // Load the last played song from AsyncStorage on component mount
   useEffect(() => {
@@ -41,6 +49,11 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
     loadLastPlayedSong();
   }, []); // Empty dependency array to run once on component mount
 
+  useEffect(() => {
+    if (isPlaying)
+      setTimeout(() => setCurrentPlayingTime(currentPlayingTime + 1), 1000);
+  }, [isPlaying]);
+
   // Function to set the current playing song
   const setPlayingSong = async (song: Song) => {
     // Don't save if the song is already saved
@@ -53,12 +66,26 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
     }
   };
 
+  const play = () => {
+    setIsPlaying(true);
+    // Logic to start playback timer or whatever you need
+  };
+
+  const pause = () => {
+    setIsPlaying(false);
+    // Logic to pause playback timer or whatever you need
+  };
+
   // Context value containing the current playing song and function to set it
   const contextValue: ContextProps = {
     currentSong,
     setPlayingSong,
     currentPlayingTime,
     setCurrentPlayingTime,
+    play,
+    pause,
+    isPlaying,
+    setIsPlaying,
   };
 
   return (
