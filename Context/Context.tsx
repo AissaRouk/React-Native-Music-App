eec60c05656915ee2958ee8fcc68cd3a3b6e37db
AissaRouk <aissa.rouk@gmail.com>
2024-02-20 13:42:59 +0200

    Added like functionalities

--- Context/Context.tsx
+++ Context/Context.tsx
@@ -13,18 +13,20 @@ interface ContextProps {
   pause: () => void;
   isPlaying: boolean;
   setIsPlaying: (bool: boolean) => void;
+  likeToggle: (song: Song) => void;
 }
 
 // Define the context
 export const AppContext = createContext<ContextProps>({
   currentSong: undefined,
-  setPlayingSong: () => {},
-  setCurrentPlayingTime: () => {},
+  setPlayingSong: () => null,
+  setCurrentPlayingTime: () => null,
   currentPlayingTime: undefined,
-  play: () => {},
-  pause: () => {},
+  play: () => null,
+  pause: () => null,
   isPlaying: false,
-  setIsPlaying: () => {},
+  setIsPlaying: () => null,
+  likeToggle: () => null,
 });
 
 // Define the ContextProvider component
@@ -64,6 +66,7 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
       } else {
         // If no songs are stored in AsyncStorage, use only the mockupSongs
         setSongList(mockupSongs);
+        await saveSongList();
       }
     } catch (error) {
       console.error("Error loading songs from AsyncStorage: ", error);
@@ -91,6 +94,31 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
     };
   }, [isPlaying, currentPlayingTime, currentSong]);
 
+  // Update current song when songList changes
+  useEffect(() => {
+    // Check if current song is in the new song list
+    console.log("Songlist has changed");
+    const currentSongInList = songList.find(
+      (song) => song.id === currentSong?.id
+    );
+    // If not, set current song to undefined
+    if (!currentSongInList) {
+      setCurrentSong(songList[0] || undefined);
+    } else {
+      setCurrentSong(currentSongInList);
+    }
+  }, [songList]);
+
+  //Save in AsyncStorage the songList
+  const saveSongList = async () => {
+    try {
+      await AsyncStorage.setItem("songs", JSON.stringify(songList));
+    } catch (error) {
+      if (error)
+        console.log("Error in saving the songs list: " + JSON.stringify(error));
+    }
+  };
+
   // Function to set the current playing song
   const setPlayingSong = async (song: Song) => {
     // Don't save if the song is already saved
@@ -113,7 +141,25 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
     // Logic to pause playback timer or whatever you need
   };
 
-  const likeToggle = (song: Song) => {};
+  const likeToggle = (song: Song) => {
+    // Find the index of the song in the songList array
+    const songIndex = songList.findIndex((s) => s.id === song.id);
+
+    // If the song is found in the songList array
+    if (songIndex !== -1) {
+      // Create a copy of the songList array to avoid mutating the state directly
+      const updatedSongList = [...songList];
+
+      // Toggle the 'like' property of the song
+      updatedSongList[songIndex] = {
+        ...updatedSongList[songIndex],
+        like: !updatedSongList[songIndex].like,
+      };
+
+      // Update the state with the modified songList array
+      setSongList(updatedSongList);
+    }
+  };
 
   // Context value containing the current playing song and function to set it
   const contextValue: ContextProps = {
@@ -125,6 +171,7 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
     pause,
     isPlaying,
     setIsPlaying,
+    likeToggle,
   };
 
   return (
