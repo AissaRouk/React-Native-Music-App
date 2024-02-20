3fe25290a4bc7f93918301a1c46b51e85d555275
AissaRouk <aissa.rouk@gmail.com>
2024-02-20 13:43:41 +0200

    Added like prop

--- Types/Song.ts
+++ Types/Song.ts
@@ -5,5 +5,6 @@ export type Song = {
   duration: number; // Duration in seconds
   album?: string; // Optional album name
   releaseYear?: number; // Optional release year
+  like?: boolean;
   // Add any other properties you want to include for a song
 };
