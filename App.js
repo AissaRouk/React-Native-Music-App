9d4939d64ec67f29e9d70b56180229ff48e2396a
AissaRouk <aissa.rouk@gmail.com>
2024-02-20 13:44:15 +0200

    Main file

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MainScreen from "./Screens/MainScreen";
+import { ContextProvider } from "./Context/Context";
+import SongModalComponent from "./Components/SongModalComponent";
 
 export default function App() {
   return (
-    <>
+    <ContextProvider>
       <StatusBar />
       <MainScreen />
-    </>
+    </ContextProvider>
   );
 }
