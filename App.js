c2ab224e23d739b24519e79c9a690691f72e75a9
AissaRouk <aissa.rouk@gmail.com>
2024-02-21 12:58:47 +0200

    Navigator added

--- App.js
+++ App.js
@@ -3,12 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 import MainScreen from "./Screens/MainScreen";
 import { ContextProvider } from "./Context/Context";
 import SongModalComponent from "./Components/SongModalComponent";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SearchScreen from "./Screens/SearchScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ContextProvider>
-      <StatusBar />
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="main"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="main" component={MainScreen} />
+          <Stack.Screen name="search" component={SearchScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ContextProvider>
   );
 }
